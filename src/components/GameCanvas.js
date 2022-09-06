c5bb960533ed82067e373c9f2501b6c36c93e71c
Brooke Heck <b19heck@gmail.com>
2022-09-06 10:52:31 -0700

    add preset permanent blocks, start destructible blocks

--- src/components/GameCanvas.js
+++ src/components/GameCanvas.js
@@ -3,16 +3,18 @@ import React, { useRef, useEffect } from 'react'
 const GameCanvas = props => {
   
   const canvasRef = useRef(null);
-  
-  const numberRowsAndCol = 20;
-  const blockSize = 30;
+  const blockSize = 50;
+  const numberRowsAndCol = 12;
   
   useEffect(() => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
 
     // create grid, you can change numberRowsAndCol and blockSize above to experiment with grid sizes
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    context.fillStyle = '#e8e8e8';
+    context.fillRect(0, 0, canvas.width, canvas.height);
+
+    // numberRowsAndCol can be changed above, this number will change the grid size
     for (let y = 0; y < numberRowsAndCol; y++) {
       for (let x = 0; x < numberRowsAndCol; x++) {
         context.strokeStyle = 'black';
@@ -20,11 +22,13 @@ const GameCanvas = props => {
       }
     }
 
+    // fills the canvas in orange wherever the permanent blocks are
     props.blockCoordinates.forEach(coord => {
-      context.fillStyle = 'red';
+      context.fillStyle = 'orange';
       context.fillRect(coord[1] * blockSize, coord[0] * blockSize, blockSize, blockSize);
     });
 
+    // fills the canvas in where the player is
     context.fillStyle = 'blue';
     context.fillRect(props.playerCoordinate[0]/2 * blockSize, props.playerCoordinate[1]/2 * blockSize, blockSize, blockSize);
   }, [props.playerCoordinate, props.blockCoordinates]);
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,17 +5,28 @@ class Main extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      playerCoordinate: [20, 20],
+      playerCoordinate: [0, 0],
+      destructibleBlocks: [],
       countdown: 5,
-      blockCoordinates: []
     }
   }
+
+  blockCoordinates = [
+    [1, 1], [1, 2], [2, 1], [2, 2], [0, 5], [0, 6], [1, 9], [1, 10], [2, 4], [2, 7], [2, 9], [2, 10],
+    [3, 5], [3, 6], [4, 5], [4, 6], [5, 0], [6, 0], [5, 3], [6, 2], [6, 3], [5, 8], [5, 9], [6, 8],
+    [5, 11], [6, 11], [7, 5], [7, 6], [8, 5], [8, 6], [9, 1], [9, 2], [9, 4], [9, 7], [9, 9], [9,10],
+    [10,1], [10, 2], [10, 9], [10, 10], [11, 5], [11, 6]
+  ];
 
   // when page loads, add the event listener, get block coordinates and start the timer
   componentDidMount() {
     document.addEventListener('keydown', (e) => { this.handleKeyPress(e) });
     this.getBlockCoordinates();
     this.startTimer();
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('keydown', (e) => { this.handleKeyPress(e) });
   }
 
   // 5 second countdown, game canvas is rendered when countdown equals 0
@@ -26,20 +37,40 @@ class Main extends React.Component {
     }, 1000)
   }
 
-  // gets 15 coordinates for the blocks that player can't hit
+  // loop through permanent blocks and make sure the coordinate does not land on a permanent block
+  checkForPermanentBlocks(x, y) {
+    return this.state.blockCoordinates.every(coord => {
+      return !(y / 2 === coord[0] && x / 2 === coord[1]);
+    });
+  }
+
+  // make sure the the destructible block doesn't land on a player, also make sure the player isn't trapped by destructible blocks
+  checkIfPlayer(x, y) {
+    let checkPlayer = (y === 0) && (x === 0);
+    let trappedXDirection = (y === 0 && x === 1) || (y === 0 && x === 2) || (y === 0 && x === 3);
+    let trappedYDirection = (y === 1 && x === 0) || (y === 2 && x === 0) || (y === 3 && x === 0); 
+    return (checkPlayer || trappedXDirection || trappedYDirection);
+  }
+
+  // get original destructible blocks
   getBlockCoordinates() {
-    let coordArr = [];
-    while (coordArr.length < 30) {
-      let x = Math.floor(Math.random() * 20);
-      let y = Math.floor(Math.random() * 20);
-      if (!(coordArr.some(coord => coord[0] === x && coord[1] === y)) && !(y === 20 && x === 20)) coordArr.push([x, y]);
+    let coordArr = [1, 2, 3, 4, 5];
+    
+    while (coordArr.length < 5) {
+      // generate two random numbers within the 12 by 12 grid
+      let x = Math.floor(Math.random() * 12);
+      let y = Math.floor(Math.random() * 12);
+
+      // check if player is where destructible block might be
+      // true means player is there, false means player is not there and will not be trapped by block
+
+      // check if the generated coordinate is the same as any of the permanent blocks
     }
-    this.setState({ blockCoordinates: coordArr });
   }
 
   checkBlock(x, y) {
-    let noBlock = this.state.blockCoordinates.every(block => {
-      return !(y/2 === block[0] && x/2 === block[1]);
+    let noBlock = this.blockCoordinates.every(block => {
+      return !(y / 2 === block[0] && x / 2 === block[1]);
     });
     return noBlock;
   }
@@ -50,7 +81,7 @@ class Main extends React.Component {
   // increment the player coordinate one in the x direction if it will not go outside the boundary
   moveRight() {
     let newCoordinate = [this.state.playerCoordinate[0] + 1, Math.ceil(this.state.playerCoordinate[1])];
-    if (this.state.playerCoordinate[0] < 38 && this.checkBlock(newCoordinate[0]+1, newCoordinate[1])) {
+    if (this.state.playerCoordinate[0] < 22 && this.checkBlock(newCoordinate[0] + 1, newCoordinate[1])) {
       this.setState({ playerCoordinate: newCoordinate });
     }
   }
@@ -58,7 +89,7 @@ class Main extends React.Component {
   // decrement the player coordinate one in the x direction if it will not go outside the boundary
   moveLeft() {
     let newCoordinate = [this.state.playerCoordinate[0] - 1, this.state.playerCoordinate[1]];
-    if (this.state.playerCoordinate[0] > 0 && this.checkBlock(newCoordinate[0]-1, newCoordinate[1])) {
+    if (this.state.playerCoordinate[0] > 0 && this.checkBlock(newCoordinate[0] - 1, newCoordinate[1])) {
       this.setState({ playerCoordinate: newCoordinate });
     }
   }
@@ -66,7 +97,7 @@ class Main extends React.Component {
   // decrement the player coordinate one in the y direction if it will not go outside the boundary
   moveUp() {
     let newCoordinate = [this.state.playerCoordinate[0], this.state.playerCoordinate[1] - 1];
-    if (this.state.playerCoordinate[1] > .1 && this.checkBlock(newCoordinate[0], newCoordinate[1]-1)) {
+    if (this.state.playerCoordinate[1] > .1 && this.checkBlock(newCoordinate[0], newCoordinate[1] - 1)) {
       this.setState({ playerCoordinate: newCoordinate });
     }
   }
@@ -74,7 +105,7 @@ class Main extends React.Component {
   // increment the player coordinate one in the y direction if it will not go outside the boundary
   moveDown() {
     let newCoordinate = [this.state.playerCoordinate[0], this.state.playerCoordinate[1] + 1];
-    if (this.state.playerCoordinate[1] < 38 && this.checkBlock(newCoordinate[0], newCoordinate[1]+1)) {
+    if (this.state.playerCoordinate[1] < 22 && this.checkBlock(newCoordinate[0], newCoordinate[1] + 1)) {
       this.setState({ playerCoordinate: newCoordinate });
     }
   }
@@ -103,7 +134,7 @@ class Main extends React.Component {
         {this.state.countdown <= 0 &&
           <GameCanvas
             playerCoordinate={this.state.playerCoordinate}
-            blockCoordinates={this.state.blockCoordinates}
+            blockCoordinates={this.blockCoordinates}
           />}
       </>
     )
